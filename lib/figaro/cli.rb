00cf0fb0cb50984035d1f56979734f9dc8780137
Bruce White <brucew@pobox.com>
2015-07-01 15:23:02 -0700

    Add Aptible support

--- lib/figaro/cli.rb
+++ lib/figaro/cli.rb
@@ -38,5 +38,29 @@ module Figaro
       require "figaro/cli/heroku_set"
       HerokuSet.run(options)
     end
+
+    # figaro aptible:set
+
+    desc "aptible:set", "Send Figaro configuration to Aptible"
+
+    method_option "app",
+                  aliases: ["-a"],
+                  desc: "Specify an Aptible app"
+    method_option "environment",
+                  aliases: ["-e"],
+                  desc: "Specify an application environment"
+    method_option "path",
+                  aliases: ["-p"],
+                  default: "config/application.yml",
+                  desc: "Specify a configuration file path"
+    method_option "remote",
+                  aliases: ["-r"],
+                  desc: "Specify an Aptible git remote"
+
+    define_method "aptible:set" do
+      require "figaro/cli/aptible_set"
+      AptibleSet.run(options)
+    end
+
   end
 end
